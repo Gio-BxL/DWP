515c01045d2083be3fb22cee2bb404855b337db6
Gio-BxL <lakegibron@gmai.com>
2022-05-25 11:37:19 -0500

    Add the index.html to the project

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//function changeColor(event){
+   //document.getElementById(main).style.backgroundColor = event.value;
+//}
+   function changeBackground(){
+    let color = document.getElementById("picker").value;
+    return color;    
+}
+function colorBackground(){
+    body.style.background = changeBackground();
+}
+
+function changeColor(){
+    let color = document.getElementById('colorInputText').style.backgroundColor = color;
+}
+
+
