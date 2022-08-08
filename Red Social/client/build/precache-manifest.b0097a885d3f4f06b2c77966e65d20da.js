be3eaf7d972fb065017a082235c5dc29314d2348
Gio-BxL <lakegibron@gmai.com>
2022-08-08 18:06:03 -0500

    chore: Add the files to the final project

---
+++ Red Social/client/build/precache-manifest.b0097a885d3f4f06b2c77966e65d20da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5441e22610374fa7b054ff8fc0f18a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7f5ac76c1bc1bcda67",
+    "url": "/static/css/main.760d0831.chunk.css"
+  },
+  {
+    "revision": "d79b369f3b0ca397eb5c",
+    "url": "/static/js/2.ee8a3179.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.ee8a3179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7f5ac76c1bc1bcda67",
+    "url": "/static/js/main.d2e8bb25.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "67fd4cbb566715d2c801b9b4b751195d",
+    "url": "/static/media/showcase.67fd4cbb.jpg"
+  },
+  {
+    "revision": "13c03d1cee8db68215f3446d8e9e7f6b",
+    "url": "/static/media/spinner.13c03d1c.gif"
+  }
+]);
