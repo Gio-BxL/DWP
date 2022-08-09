c51c25d385c363d74f429cb1513ee20647ea13aa
Gio-BxL <lakegibron@gmai.com>
2022-08-08 19:00:38 -0500

    feat: Put the image in background

--- Red Social/client/src/components/layout/Landing.js
+++ Red Social/client/src/components/layout/Landing.js
@@ -15,10 +15,9 @@ const Landing = ({ isAuthenticated }) => {
       <section className='landing'>
         <div className='dark-overlay'>
           <div className='landing-inner'>
-            <h1 className='x-large'>DevNet</h1>
+            <h1 className='x-large'>Mi Red Social</h1>
             <p className='lead'>
-              Create a developer profile/portfolio, share posts and get help
-              from other developers
+              
             </p>
             <div className='buttons'>
               <Link to='/register' className='btn btn-primary'>
